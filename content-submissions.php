49d1a2c0c53d6a47b7ccfa8ce69d19a01bb6ac34
svsdesign <simon@svsdesign.co.uk>
2021-02-16 18:16:36 +0000

    no message

--- content-submissions.php
+++ content-submissions.php
@@ -11,72 +11,51 @@
 ?>
  
 
-<?php if ( is_user_logged_in() ):
+<?php if ( is_user_logged_in() ): 
 
 the_content();?>
 
- <?php else: ?>
+<?php else:
 
-      <?php
-      $image_data = get_field('image-data_hidden_field'); //text
-      $submission_name = get_field('submission_name'); //text
+    $image_data = get_field('image-data_hidden_field'); //text
+    $submission_name = get_field('submission_name'); //text
+    $submission_description = get_field('your_park_description'); //text
+    $submission_suggestion = get_field('your_park_suggestion'); //text
+    $submission_email = get_field('your_park_suggestion'); //text
 
-      $submission_description = get_field('your_park_description'); //text
-      $submission_suggestion = get_field('your_park_suggestion'); //text
-
-/*
-Edit Duplicate Move Delete
-Text Area
-2
-Suggestion Message
-Text Area
-3
-Image Test
-an_image_testImage
-4
-Name
-Text
-5
-Email
-submission_email
-*/
-
-
-                       
       if ( $image_data ) : ?>
-
-      <?php 
-      //echo     $test; 
-      echo '<img class="submission-image" src="'.$image_data.'"/>';
-      ?>
-      <?php endif;
+</br>
+      <?php echo '<img class="submission-image" src="'.$image_data.'"/>';
+     
+      endif;
       // echo '<img src="data:image/jpeg;base64,'.base64_encode($test).'">';
         ?>
 
     <?php if ($submission_name):?>
-    
-      <div class="submission-name">
+      </br>
+      <h2 class="submission-name">
         <?php echo $submission_name;?>
-      </div>
+    </h2></br>
 
     <?php endif; ?>
 
     <?php if ($submission_description):?>
-    
+      <div class="submission-header"> Description</div>
+     
       <div class="submission-description">
         <?php echo $submission_description;?>
-      </div>
+      </div></br>
 
     <?php endif; ?>
 
     <?php if ($submission_suggestion):?>
-    
+      <div class="submission-header"> Suggestion</div>
     <div class="submission-suggestion">
       <?php echo $submission_suggestion;?>
-    </div>
+    </div></br>
 
   <?php endif; ?>
 
-    <?php //endif; ?>
+<?php endif; ?>
 
-  <?php endif;?>
+ 
