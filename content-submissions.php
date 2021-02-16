a8713ce383492e44ff5eec00d596c493c702c6a2
svsdesign <simon@svsdesign.co.uk>
2021-02-16 17:38:24 +0000

    fixing visbility of submissions

--- content-submissions.php
+++ content-submissions.php
@@ -19,20 +19,64 @@ the_content();?>
 
       <?php
       $image_data = get_field('image-data_hidden_field'); //text
-                              
+      $submission_name = get_field('submission_name'); //text
+
+      $submission_description = get_field('your_park_description'); //text
+      $submission_suggestion = get_field('your_park_suggestion'); //text
+
+/*
+Edit Duplicate Move Delete
+Text Area
+2
+Suggestion Message
+Text Area
+3
+Image Test
+an_image_testImage
+4
+Name
+Text
+5
+Email
+submission_email
+*/
+
+
+                       
       if ( $image_data ) : ?>
 
       <?php 
       //echo     $test; 
-
-
       echo '<img class="submission-image" src="'.$image_data.'"/>';
       ?>
-
-      <?php 
+      <?php endif;
       // echo '<img src="data:image/jpeg;base64,'.base64_encode($test).'">';
-      ?>  
+        ?>
 
-  <?php endif; 
+    <?php if ($submission_name):?>
+    
+      <div class="submission-name">
+        <?php echo $submission_name;?>
+      </div>
 
-endif;?>
+    <?php endif; ?>
+
+    <?php if ($submission_description):?>
+    
+      <div class="submission-description">
+        <?php echo $submission_description;?>
+      </div>
+
+    <?php endif; ?>
+
+    <?php if ($submission_suggestion):?>
+    
+    <div class="submission-suggestion">
+      <?php echo $submission_suggestion;?>
+    </div>
+
+  <?php endif; ?>
+
+    <?php //endif; ?>
+
+  <?php endif;?>
